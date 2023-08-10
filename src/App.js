bce138820d18d4012958758e411b5c24c2ec328a
dupe <marcojurgenjuras@gmail.com>
2023-08-10 15:55:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Kokot ono to funguje
         </p>
         <a
           className="App-link"
