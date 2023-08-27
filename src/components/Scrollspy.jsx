74ce7b6c0cff753ed4eff9cb5d4a83ef74e799fe
dupe <marcojurgenjuras@gmail.com>
2023-08-27 21:48:12 +0200

    Fixed logo + Collapse WIP// TODO: off-canvas collapse fix bug when clicking on button

--- src/components/Scrollspy.jsx
+++ src/components/Scrollspy.jsx
@@ -3,61 +3,74 @@ import React from "react";
 const Scrollspy = () => {
   return (
     <>
-      <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
-        <a class="navbar-brand" href="/">
+      <nav id="navbar-example2" class="navbar bg-body-tertiary ">
+        <a
+          class="navbar-brand d-flex justify-content-start "
+          href="/"
+          // style={{ width: "30%", height: "36%" }}
+        >
           <img
             src={require("../Sj_transportlogo.jpg")}
-            class="logo"
+            class="logo  "
             alt="SJ Transport"
-            style={{ width: "8%", height: "8%" }}
+            style={{ width: "130px", height: "18" }}
             // width="30"
             // height="24"
           ></img>
         </a>
-
-        <ul class="nav nav-pills">
-          <li class="nav-item">
-            <a class="nav-link" href="#scrollspyHeading1">
-              First
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#scrollspyHeading2">
-              Second
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              data-bs-toggle="dropdown"
-              href="/"
-              role="button"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="#scrollspyHeading3">
-                  Third
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#scrollspyHeading4">
-                  Fourth
-                </a>
-              </li>
-              <li>
-                <hr class="dropdown-divider"></hr>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#scrollspyHeading5">
-                  Fifth
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="nav nav-pills d-flex justify-content-end ">
+            <li class="nav-item">
+              <a class="nav-link " href="#scrollspyHeading1">
+                First
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#scrollspyHeading2">
+                Second
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle "
+                data-bs-toggle="dropdown"
+                href="/"
+                role="button"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item " href="#scrollspyHeading3">
+                    Third
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item " href="#scrollspyHeading4">
+                    Fourth
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item " href="#scrollspyHeading5">
+                    Fifth
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </nav>
       <div
         data-bs-spy="scroll"
@@ -69,7 +82,6 @@ const Scrollspy = () => {
       >
         <h4 id="scrollspyHeading1">First heading</h4>
         <p>
-          <img src="./src/Sj_transportlogo.jpg" alt="SJ Transport"></img>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
